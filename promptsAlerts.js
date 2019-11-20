3b8223f9912e220e48a0939fb36b564812cb50a8
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-20 11:46:23 +0000

    reupload

---
+++ promptsAlerts.js
@@ -0,0 +1,14 @@
+console.log("Hello from my JS file promptsAlerts.js.")
+
+let greeting = "Hi everyone"
+alert(greeting);
+
+let favouriteGame = prompt("What is your favourite game?")
+let result = favouriteGame.toLowerCase();
+console.log(favouriteGame);
+
+if (result == "battlefield") {
+    alert("Well done for playing the f***ing objective!")
+} else {
+    alert("Why aren't you playing Battlefield?")
+}
